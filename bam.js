20b5b44b5e9f7b6c5edaa2bbd32e223994147f9d
Tim Burns <burnstimothy87@gmail.com>
2020-02-16 22:26:26 -0500

    BAMMM

--- bam.js
+++ bam.js
@@ -28,3 +28,79 @@ function loadProducts() {
         promptCustomerForItem(res);
     });
 }
+
+function promptCustomerForItem(inventory) {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "choice",
+            message: "What is the ID of the item you would you like to purchase? [Quit with Q]",
+            validate: function (val) {
+                return !isNaN(val) || val.toLowerCase() === "q";
+            }
+        }])
+        .then(function (val) {
+            checkIfShouldExit(val.choice);
+            let choiceId = parseInt(val.choice);
+            let product = checkInventory(choiceId, inventory);
+            if (product) {
+                promptCustomerForQuantity(product);
+            } else {
+                console.log("\nThat item is not in the inventory.");
+                loadProducts();
+            }
+        });
+}
+
+// Prompt the customer for a product quantity
+function promptCustomerForQuantity(product) {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "quantity",
+            message: "How many would you like? [Quit with Q]",
+            validate: function (val) {
+                return val > 0 || val.toLowerCase() === "q";
+            }
+        }])
+        .then(function (val) {
+            checkIfShouldExit(val.quantity);
+            let quantity = parseInt(val.quantity);
+
+            if (quantity > product.stock_quantity) {
+                console.log("\nInsufficient quantity!");
+                loadProducts();
+            } else {
+                makePurchase(product, quantity);
+            }
+        });
+}
+
+function makePurchase(product, quantity) {
+    connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+        [quantity, product.item_id],
+        function (err, res) {
+            console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
+            loadProducts();
+        }
+    );
+}
+
+
+function checkInventory(choiceId, inventory) {
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].item_id === choiceId) {
+            return inventory[i];
+        }
+    }
+
+    return null;
+}
+
+function checkIfShouldExit(choice) {
+    if (choice.toLowerCase() === "q") {
+        console.log("Goodbye!");
+        process.exit(0);
+    }
+}
